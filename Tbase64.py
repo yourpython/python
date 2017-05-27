71215c951df5bf696618799d0cdd415ec0f31f59
yourpython <1529100174@qq.com>
2017-05-27 16:21:50 +0800

    second modify

--- Tbase64.py
+++ Tbase64.py
@@ -2,12 +2,26 @@
 #coding=utf-8
 import base64
 
-a = input("Enter string:")
-b = base64.b64encode(a)  # Encode for string
+h ='haha'
+str =h.encode('utf-8')
+print (str)
+str1= base64.b64encode(str)  # Encode for string
+print(u'转码base64后的haha：'+str1.decode()) #打印64
+deco = base64.b64decode(str1)
+print (deco)
+print (u'转为utf-8的haha：'+deco.decode('utf-8'))
 
-print (base64(b))  # Decode for string
+"""f = open("1111.docx",'rb')
+a = f.read()
+b=base64.b64decode(a)
+f.close()
+b1=b.decode('utf-8')
+file1 = open("123.txt",'wb')
+file1.write(b1)
+file1.close()
+print  (b1)"""
 
-f = open('temp.jpg', 'rb')
+"""f = open('temp.jpg', 'rb')
 c = f.read()
 
 x = base64(c)  # Encode for file
@@ -19,4 +33,4 @@ print
 
 base64(x)  # Decode for file
 file1 = open('temp', 'wb')
-file1.write(base64(x))
+file1.write(base64(x))"""
